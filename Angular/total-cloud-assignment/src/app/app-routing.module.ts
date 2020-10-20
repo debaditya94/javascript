ead5b6f0286b03b3b95bcccaaee33db39bfc6bb2
debaditya94 <dchoudhury221194@gmail.com>
2020-10-20 20:10:40 +0530

    Navigation routes added

--- Angular/total-cloud-assignment/src/app/app-routing.module.ts
+++ Angular/total-cloud-assignment/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './screens/home/home.component';
+import { DetailsComponent } from './screens/details/details.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'details',
+    component: DetailsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
