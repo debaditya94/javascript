0822a31d7d23975603a0c4209d468bd11c2e2bb0
debaditya94 <dchoudhury221194@gmail.com>
2020-10-20 20:05:07 +0530

    Added home & details component

--- Angular/total-cloud-assignment/src/app/app.module.ts
+++ Angular/total-cloud-assignment/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './screens/home/home.component';
+import { DetailsComponent } from './screens/details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
