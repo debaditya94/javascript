5a6b1b8ff62c990e3e8336cb14d174202df33f25
debaditya94 <dchoudhury221194@gmail.com>
2020-11-01 13:14:33 +0530

    creation & loading  crawled movie data list to a separate json file

--- FrontEnd+Backend/React+Node/server/index.js
+++ FrontEnd+Backend/React+Node/server/index.js
@@ -1,15 +1,26 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 const url = "https://www.imdb.com/search/title/?count=100&groups=top_1000&sort=user_rating";
+const fs = require('fs');
 
 fetchData(url).then( (res) => {
     const html = res.data;
     const $ = cheerio.load(html);
     const moviesList = $('body > #wrapper > #root > .pagecontent > .redesign > #main > .article > .lister.list.detail.sub-list > .lister-list > .lister-item.mode-advanced');
+    const moviesListData = [];
     moviesList.each(function() {
         // console.log(this);
-        let title = $(this).find("div[class='lister-item-content'] > h3[class='lister-item-header'] > a").text().trim();
+        const title = $(this).find("div[class='lister-item-content'] > h3[class='lister-item-header'] > a").text().trim();
+        const movieInfo = {
+            title: title
+        };
+        moviesListData.push(movieInfo);
         console.log(title);
+    });
+    const fileData = JSON.stringify(moviesListData, null, 2);
+    fs.writeFile('movie-list.json', fileData, (err) => {
+        if (err) throw err;
+        console.log('Movie data written to file');
     });
 })
 
